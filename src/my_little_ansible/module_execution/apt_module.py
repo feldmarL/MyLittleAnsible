882a3c43df21d3fd1dd29b284d67955f48b08c6f
feldma_l <feldma_l@etna-alternance.net>
2023-11-28 19:38:22 +0100

    Refacto to get rid of global variables.

--- src/my_little_ansible/module_execution/apt_module.py
+++ src/my_little_ansible/module_execution/apt_module.py
@@ -4,43 +4,55 @@ Python module to execute apt module defined in todos file.
 
 from .tools import execute_command, logger
 
-PACKAGE: str
-ACTION: str
+def switch_state(params, original):
+    """ Switch state as we need install and remove in code
+        but don't want to change todo.
 
-def set_variables_and_check_ok(params, client, host_ip):
-    """ Set variables from received parameters and check if
-        the action is already done and return state + variables.
+    Args:
+        params (list(String)): List of parameters about package defined in todos file.
+        original (bool): Action type. Back to original or not?
+
+    Returns:
+        params (list(String)): List of parameters about package defined in todos file.
+    """
+    if not original:
+        if params["state"] == "present":
+            params["state"] = "install"
+        elif params["state"] == "absent":
+            params["state"] = "remove"
+
+    if original:
+        if params["state"] == "install":
+            params["state"] = "present"
+        elif params["state"] == "remove":
+            params["state"] = "absent"
+
+    return params
+
+def check_state(client, params, host_ip):
+    """ Check if the action is already done and return state + variables.
 
     Args:
         params (list(String)): List of parameters about package defined in todos file.
         client (SSHClient): Paramiko's SSH Client used to connect to host.
         host_ip (int): Host ip.
-        logger (Logger): The main created logger to log.
 
     Returns:
-        state, action, package (String, String, String):
-            Execution state, action to do and package on which action to do.
+        state (String): Execution state, action to do and package on which action to do.
     """
-    global PACKAGE, ACTION
-    PACKAGE = params["name"]
-    if params["state"] == "present":
-        ACTION = "install"
-    elif params["state"] == "absent":
-        ACTION = "remove"
-
-    command = f'dpkg -s {PACKAGE} | grep "Status"'
+    command = f'dpkg -s {params["name"]} | grep "Status"'
     stdout, _ = execute_command(False, client, command)
 
-    logger.debug(f"{PACKAGE} state on {host_ip}: {stdout}")
+    logger.debug(f"{params['name']} state on {host_ip}: {stdout}")
 
-    if ACTION == "install":
+    if params["state"] == "install":
         if "ok installed" in stdout:
-            logger.info(f"{PACKAGE} already installed on {host_ip}. Todos DONE with status OK.")
+            logger.info(f"{params['name']} already installed on {host_ip}. Todos DONE with status OK.")
             return "ok"
 
-    elif ACTION == "remove":
+    elif params["state"] == "remove":
         if not "ok installed" in stdout:
-            logger.info(f"{PACKAGE} already uninstalled on {host_ip}. Todos DONE with status OK.")
+            logger.info(f"{params['name']} already uninstalled on {host_ip}. Todos DONE with status OK.")
             return "ok"
 
     return "to change"
@@ -67,18 +79,22 @@ def apt(client, params, host_pwd, host_ip):
         logger.error(f"No password were provided in inventory file for {host_ip}. "
                      f"apt module can't be executed without sudo password.")
         return "ko"
-    state = set_variables_and_check_ok(params, client, host_ip)
 
+    params = switch_state(params, False)
+    
+    state = check_state(client, params, host_ip)
     if state == "ok":
         return state
 
-    command = (f'echo "{host_pwd}" | sudo -S apt-get -y {ACTION} {PACKAGE}')
+    command = (f'echo "{host_pwd}" | sudo -S apt-get -y {params["state"]} {params["name"]}')
     stdout, stderr = execute_command(True, client, command, host_pwd=host_pwd)
 
-    logger.debug(f"While trying to {ACTION} {PACKAGE}, STDOUT:\n{stdout}")
+    logger.debug(f"While trying to {params['state']} {params['name']}, STDOUT:\n{stdout}")
 
     if stderr != "" and "dpkg-preconfigure: unable to re-open stdin:" not in stderr:
-        logger.error(f"While trying to {ACTION} {PACKAGE}, first 200 STDERR chars:\n{stderr[:200]}")
+        logger.error(f"While trying to {params['state']} {params['name']}, first 200 STDERR chars:\n{stderr[:200]}")
+        params = switch_state(params, True)
         return "ko"
 
+    params = switch_state(params, True)
     return "changed"
